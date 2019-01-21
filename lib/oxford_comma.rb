2219fba01facf8793df184980a7d1dc24600c587
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-21 21:34:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+ if array.length == 1
+   return array.join()
+   
+ elsif array.length == 2
+  return array.join(" and ")
+  
+  
+elsif array.length >= 3
+newList =""
 
+lastItem = array[-1]
+array.slice!(-1)
+
+newList << array.join(', ')+", and " + lastItem
+return newList
+  end
 end
