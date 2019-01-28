78c9f40c177c4db4435553c9aa61b4c33c949a6e
Alonso Ortega <alonsojortega@gmail.com>
2019-01-28 00:18:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     return array[0] + " and " + array[1]
   elsif array.length > 2
-    array[-1] = "and #{arr[-1]}"
+    array[-1] = "and #{array[-1]}"
   end
 
   array.join(", ")
